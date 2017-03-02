01117fa9613d33c1ba13394a4f53c22db6eee657
duan <236976504@qq.com>
2017-03-02 14:07:14 +0800

    项目

---
+++ dlg/app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        //显示后台的首页
+        return view('admins.index');
+    }
+}
